e5adbdb280b187f4af8eecce3544cc010d1b3c6c
Nguyễn Phạm Trường Duy <2001220723@hufi.edu.vn>
2025-11-15 19:10:35 +0700

    fix: css

--- resources/views/components/blog/side-video.blade.php
+++ resources/views/components/blog/side-video.blade.php
@@ -2,7 +2,7 @@
     <h3 class="widget-title"><i class="fa fa-play-circle"></i> Video nổi bật</h3>
     <div class="widget-content">
         <div class="video-thumb">
-            <a href="https://www.youtube.com/@truongduy8517" target="_blank">
+            <a href="https://www.youtube.com/@vtv24" target="_blank">
                 <img src="{{ asset('storage/placeholders/placeholder-image.jpg') }}" 
                      onerror="this.src='https://via.placeholder.com/400x250/dc3545/ffffff?text=Video+Hot'" 
                      alt="Video nổi bật">
@@ -10,7 +10,7 @@
                     <i class="fa fa-play-circle"></i>
                 </div>
             </a>
-            <h4><a href="https://www.youtube.com/@truongduy8517" target="_blank">Xem video tin tức mới nhất</a></h4>
+            <h4><a href="https://www.youtube.com/@vtv24" target="_blank">Xem video tin tức mới nhất</a></h4>
         </div>
     </div>
 </div>
